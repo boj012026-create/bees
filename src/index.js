958fc153a83db5894cde5820cb738e6e2e4ef446
boj012026-create <boj012026@gmail.com>
2026-06-29 14:55:53 +0200

    feat(api): add bees/id

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-import express, { Router json } from 'express';
+import express, { Router } from 'express';
+
+const app = express();
+const HOST = '127.0.0.1';
+const PORT = '3002';
 
 const beeController = {
     'getId': (req, res) => {
-        res.send(req.id)
+        res.send(req.params.wingNr);
     }
 }
+
+const beeRouter = Router();
+
+beeRouter.get('/:wingNr', beeController['getId']);
+
+app.use('/bees', beeRouter)
+
+app.listen(PORT, HOST, (err) => {
+    if (err) {
+        console.error("------- FATAL ERROR -------");
+        console.error(err);
+        return -1;
+    }
+
+    console.log(`listening on ${HOST}:${PORT}`);
+});
